a8d991ddc2fe71ec6b7121c432469576dc6af9e1
Etesami <etesamikowsar@gmail.com>
2025-05-24 02:47:55 +0330

    update signup

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -40,6 +40,53 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return instance
     
 class SignupSerializer(serializers.ModelSerializer):
+
+    # Defining additional fields.
+    
+    full_name = serializers.CharField(write_only=True)
+    age = serializers.IntegerField(write_only=True, required=False)
+    national_code = serializers.IntegerField(write_only=True, required=False)
+    phone_number = serializers.CharField(write_only=True, required=False)
+
+
+    #Password confirmation
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ["username", "email", "password", "password2", "full_name", "age", "national_code", "phone_number"]
+
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError({"error": "رمز عبور با تکرار مطابقت ندارد."})
+        return data
+    
+    def create(self, validated_data):
+
+        #  Separating profile fields from the user.
+        full_name = validated_data.pop("full_name")
+        age = validated_data.pop("age", None)
+        national_code = validated_data.pop("national_code", None)
+        phone_number = validated_data.pop("phone_number", None)
+        validated_data.pop("password2")
+
+        #create user. The (create_user) method hashes the password.
+        user = User.objects.create_user(
+            username = validated_data["username"],
+            email = validated_data["email"],
+            password = validated_data["password"]
+        )
+
+        #create user profile.
+        profile = UserProfile.objects.create(
+            user=user,
+            full_name=full_name,
+            age=age,
+            National_code=national_code,
+            phone_number=phone_number,
+            is_student=True,
+
+        )
+
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -2,26 +2,33 @@ from django.shortcuts import render
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import UserProfile , User
-from .serializers import UserProfileSerializer, UserSerializer , SignupSerializer
+from .serializers import UserProfileSerializer, SignupSerializer
 from rest_framework import status , viewsets, mixins, generics
 from rest_framework.permissions import AllowAny
-
+from rest_framework.views import APIView
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
 
-class SignupView(mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = User.objects.order_by('id')
-    serializer_class = SignupSerializer
+class SignupView(APIView):
     permission_classes = [AllowAny]
-
     def post(self, request: Request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = SignupSerializer(data = request.data)
         if serializer.is_valid():
             user = serializer.save()
-            UserProfile.objects.create(user=user)
-
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                "status": 200,
+                "date":{
+                    "username": user.username,
+                    "email": user.email,
+                },
+                "error": None
+            }, status=status.HTTP_200_OK)
+        
+        return Response({
+            "status": 400,
+            "data": None,
+            "error": serializer.errors
+        }, status= status.HTTP_400_BAD_REQUEST)
