eea84f74bb1eefaf4271b1bf68e3a8578ecb5886
Etesami <etesamikowsar@gmail.com>
2025-05-24 22:53:47 +0330

    update profile views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import  SignupView
+from .views import  SignupView, UserProfileDetailApiView, UserProfileView
 from .import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-router = DefaultRouter()
-router.register('', views.UserProfileViewSet)
-
 
 urlpatterns = [
-    path('profiles/', include(router.urls)),
+    
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('profiles/', views.UserProfileView.as_view() ),
+    path('profiles/<int:profile_id>', views.UserProfileDetailApiView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,90 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import UserProfile , User
 from .serializers import UserProfileSerializer, SignupSerializer
-from rest_framework import status , viewsets, mixins, generics
+from rest_framework import status 
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
-class UserProfileViewSet(viewsets.ModelViewSet):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+class UserProfileView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request: Request):
+        profiles = UserProfile.objects.order_by('id').all()
+        ProfileSerializer = UserProfileSerializer(profiles, many=True)
+        return Response({
+                "status": 200,
+                "date": ProfileSerializer.data,
+                "error": None
+            }, status=status.HTTP_200_OK)
+    
+
+    def post(self, request: Request):
+        
+        creatprofileserializer = UserProfileSerializer(data = request.data)
+        if creatprofileserializer.is_valid(): 
+            creatprofileserializer.save()
+            return Response({
+                "status": 200,
+                "date":creatprofileserializer.data,
+                "error": None
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+            "status": 400,
+            "data": None,
+            "error": creatprofileserializer.errors
+        }, status= status.HTTP_400_BAD_REQUEST)
+
+class UserProfileDetailApiView(APIView):
+    permission_classes = [AllowAny]
+    def get_object(self, profile_id:int):
+        try:
+            profile = UserProfile.objects.get(pk=profile_id)
+            return profile
+        
+        except UserProfile.DoesNotExist:
+            return Response({
+            "status": 400,
+            "data": None,
+            "error": None
+        }, status= status.HTTP_400_BAD_REQUEST)
+        
+
+    def get(self, request: Request, profile_id:int):
+        profile = self.get_object(profile_id)
+        serializer = UserProfileSerializer(profile)
+        return Response({
+                "status": 200,
+                "date":serializer.data,
+                "error": None
+            }, status=status.HTTP_200_OK)
+    
+
+    def put(self, request: Request, profile_id:int):
+        profile = self.get_object(profile_id)
+        serializer = UserProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "status": 200,
+                "date":serializer.data,
+                "error": None
+            }, status=status.HTTP_200_OK)
+        
+        return Response({
+            "status": 400,
+            "data": None,
+            "error": serializer.errors
+        }, status= status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, profile_id:int):
+        profile = self.get_object(profile_id)
+        profile.delete()
+        return Response({
+                "status": 200,
+                "date":None,
+                "error": None
+            }, status=status.HTTP_200_OK)
+
 
 
 class SignupView(APIView):
